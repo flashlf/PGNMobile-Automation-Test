e2f35a1738d16518fc88e9d0b8815e111d79e9da
flashlf <flash.lavon@hotmail.com>
2024-09-11 23:20:57 +0700

    feat: basic feature open apps (main)

---
+++ src/main/java/pgn/mobile/appiumproject/Main.java
@@ -0,0 +1,67 @@
+package pgn.mobile.appiumproject;
+
+import io.appium.java_client.flutter.android.FlutterAndroidDriver;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pgn.mobile.appiumproject.constants.AppConstants;
+
+/**
+ * @author M.I.A
+ * Automation UI Testing for PGNMobile using Appium 
+ * Version 1.0
+ * 11 September 2024
+ */
+
+public class Main {
+    
+    static FlutterAndroidDriver driver;
+    static DesiredCapabilities dCap;
+    static WebDriverWait wait; 
+
+    public static void main(String[] args) {
+        try {
+            initiateDevice();
+        } catch (Exception e) {
+            System.out.println(e.getCause());
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void initiateDevice() throws Exception {
+        dCap = new DesiredCapabilities();
+        dCap.setCapability("appium:deviceName", AppConstants.CAP_DEVICE);
+        dCap.setCapability("appium:udid", AppConstants.CAP_UDID);
+        dCap.setCapability("platformName", AppConstants.CAP_PLATFORM_NAME);
+        dCap.setCapability("appium:platformVersion", AppConstants.CAP_PLATFORM_VERSION);
+        dCap.setCapability("appium:appPackage", AppConstants.CAP_APP_PACKAGE);
+        dCap.setCapability("appium:appActivity", AppConstants.CAP_APP_ACTIVITY);
+        dCap.setCapability("appium:automationName", AppConstants.CAP_AUTOMATION_NAME);
+        // dCap.setCapability("appium:noReset", true); // Clear Cache ?
+        
+        driver = new FlutterAndroidDriver(AppConstants.HOST_URL, dCap);
+        System.out.println("Testing Appium Started");
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Duration waitTime = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, waitTime);
+        
+        // Permission Apps
+        WebElement permission1 = driver.findElement(By.xpath(AppConstants.ANDROID_LOCATION_PERMISSION));
+        wait.until(ExpectedConditions.visibilityOf(permission1));
+        permission1.click();
+        
+        WebElement permission2 = driver.findElement(By.xpath(AppConstants.ANDROID_LOCATION_PERMISSION));
+        wait.until(ExpectedConditions.visibilityOf(permission2));
+        permission2.click();
+        
+        WebElement permission3 = driver.findElement(By.xpath(AppConstants.ANDROID_NOTIFICATION_PERMISSION));
+        wait.until(ExpectedConditions.visibilityOf(permission3));
+        permission3.click();
+    }
+}
---
+++ src/main/java/pgn/mobile/appiumproject/constants/AppConstants.java
@@ -0,0 +1,52 @@
+package pgn.mobile.appiumproject.constants;
+
+import java.net.URL;
+
+/**
+ *
+ * @author REDHAT
+ * List Constant yang dibutuhkan pada apps
+ */
+public final class AppConstants {
+    
+    private AppConstants() {}
+    
+    /** GENERAL CONFIG **/
+    public static final String ACC_PHONE = "";
+    public static final String ACC_PASS = "";
+    public static final URL HOST_URL = new URL("http://127.0.0.1:4723/");
+    public static final String CAP_DEVICE = ""; // your device name
+    public static final String CAP_UDID = ""; // Unique Id in adb device command
+    public static final String CAP_PLATFORM_NAME = "Android"; // Android | IOS
+    public static final String CAP_PLATFORM_VERSION = "14";
+    public static final String CAP_APP_PACKAGE = "com.pgn.customer";
+    public static final String CAP_APP_ACTIVITY = "com.pgn.customer.MainActivity";
+    public static final String CAP_AUTOMATION_NAME = "UiAutomator2"; // UiAutomator2 = Android | Xcuitest = IOS
+
+    /** ANDROID PERMISSION **/
+    public static final String ANDROID_LOCATION_PERMISSION = "//android.widget.Button[@resource-id=\"com.android.permissioncontroller:id/permission_allow_foreground_only_button\"]";
+    public static final String ANDROID_CAMERA_PERMISSION = "//android.widget.Button[@resource-id=\"com.android.permissioncontroller:id/permission_allow_foreground_only_button\"]";
+    public static final String ANDROID_NOTIFICATION_PERMISSION = "//android.widget.Button[@resource-id=\"com.android.permissioncontroller:id/permission_allow_button\"]";
+    
+    /**  LIST XPATH **/
+    // Onboard Screen
+    public static final String XPATH_ONBOARD_BTN_LOGIN = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[3]";
+    public static final String XPATH_ONBOARD_BTN_REGISTER = "";
+    
+    // Login Screen
+    public static final String XPATH_LOGIN_PHONE = "//android.widget.ScrollView/android.widget.EditText[1]";
+    public static final String XPATH_LOGIN_PASSWORD = "//android.widget.ScrollView/android.widget.EditText[2]";
+    public static final String XPATH_LOGIN_BTN_SUBMIT = "//android.widget.ScrollView/android.view.View[6]";
+    
+    // Otp Login Screen
+    public static final String XPATH_OTP_PIN_FIELD = "//android.widget.EditText";
+    public static final String XPATH_OTP_BTN_SUBMIT = "//android.widget.ScrollView/android.view.View[7]";
+    
+    // Dashboard Screen
+    public static final String XPATH_TOUR_STEP1_SKIP = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View[5]";
+    public static final String XPATH_TOUR_STEP1_CONTINUE = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View[6]";
+    
+    // 618,1122 Step1 Skip Pointer
+    // 973, 1117 Step1 Continue
+    
+}
